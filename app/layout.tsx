dbbb78900d57bff021b69214a5ad7fbbea5f43f3
Aman <amannathpal@gmail.com>
2024-08-13 15:51:17 +0530

    update meetingsetup file

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ import "react-datepicker/dist/react-datepicker.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "EchoMeet",
+  title: "EchoMeet  ",
   description: "Video calling App",
   icons: {
     icon: "/icons/logo.svg",
